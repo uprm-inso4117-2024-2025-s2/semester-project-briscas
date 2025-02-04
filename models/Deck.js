021e2fba5938581bb53ce71a1c34ba35086e2852
Saul-Caban-UPRM <93611806+Saul-Caban-UPRM@users.noreply.github.com>
2025-02-04 14:05:34 -0400

    Created trump card mechanics and added tester

--- models/Deck.js
+++ models/Deck.js
@@ -3,9 +3,42 @@ const Card = require('./Card'); // Import the Card class
 class Deck {
     constructor() {
         this.deck = []; // Stores the 40 unique cards
+        this.trumpSuit = null; //Stores Trump suit
+        this.trumpCard = null; //Stores Trump card
         this.initializeDeck(); // Generate the deck
     }
+    //Getters and Setters
+    // Get the trump suit
+    getTrumpSuit() {
+        return this.trumpSuit;
+    }
 
+    // Get the trump card
+    getTrumpCard() {
+        return this.trumpCard;
+    }
+
+    // Check deck size
+    getDeckSize() {
+        return this.deck.length;
+    }
+
+    //Draws a card 
+    getCard() {
+        return this.deck.pop();
+    }
+
+    //Set up the trump suit
+    setupTrumpSuit() {
+        if (!this.trumpCard) {
+            this.trumpCard = this.getCard();
+            this.trumpSuit = this.trumpCard.suit;
+        }
+        return this.trumpCard;
+    }
+    
+    //Functions
+    
     // Initialize the deck by creating 40 unique cards
     initializeDeck() {
         const suits = ["Oros", "Copas", "Espadas", "Bastos"];
@@ -14,11 +47,6 @@ class Deck {
         this.deck = suits.flatMap(suit =>
             ranks.map(rank => new Card(suit, rank))
         ).filter(card => card.suit && card.rank); // Remove invalid cards
-    }
-
-    // Check deck size
-    getDeckSize() {
-        return this.deck.length;
     }
 
     // Method to display all cards in the deck
