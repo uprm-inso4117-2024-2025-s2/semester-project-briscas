fd3c18a0a08fb721bab94307e13f1dba36dbc1d4
Alej24 <alejandro.rivera24@upr.edu>
2025-02-03 21:02:58 -0400

    Card class has been implemented.

---
+++ cards/CardTest.js
@@ -0,0 +1,13 @@
+const Card = require("./Card");
+
+const card1 = new Card("Copas", "3");  // Displays the card 3 of Copas.
+console.log(card1.displayCard()); 
+
+const card2 = new Card("Diamantes", "1");  // Displays a warning message since the rank is invalid.
+console.log(card2.displayCard());
+
+const card3 = new Card("Oros", "8");  // Displays a warning message since the rank is invalid.
+console.log(card3.displayCard());
+
+const card4 = new Card("Corazones", "9");  // Displays a warning message since the suit and rank are invalid.
+console.log(card4.displayCard());
