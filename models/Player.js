b90a66e3dd5e47e460bbfe1cad8266d10791546e
sotam1069 <pedro.matos9@upr.edu>
2025-02-20 19:08:02 -0500

    Fix and Refactor Player.js file

--- models/Player.js
+++ models/Player.js
@@ -1,20 +1,21 @@
 const Card = require('./Card');
 
 class Player{
-    constructor(hand, score, isTurn, cardMax) {
+    constructor(hand = [], score = 0, isTurn = false, cardMax = 10) {
         this.hand = hand;
-        //defined as an typedArray with given maximum number of cards
+        //defined as an array with given maximum number of cards
         this.score = score;
         //defined as a int
         this.isTurn = isTurn;
         //defined as a boolean
         this.canDraw = true;
         //defined as a boolean
+        this.cardMax = cardMax;
 
     }
     temporaryHand = null;
     giveHand(){
-        return hand;
+        return this.hand;
         //Return the given hand of the player.
     }
     setScore(newScore){
@@ -22,14 +23,17 @@ class Player{
         //Set the score of player to the given ammount.
     }
     addScore(newScore){
-        this.score =+ newScore;
+        this.score += newScore;
         //Alternative to .setScore(), Adds the given ammount to the score of the player.
     }
     getScore(){
         return this.score;
     }
     draw(Deck) {
-        const drawnCard = Deck.draw()
+        
+        if(!Deck || typeof Deck.draw !== 'function') {
+            throw new Error("Invalid deck provided.");
+        }
         if (!this.isTurn) {
             //checks if it's not the player's turn
             throw new Error("It is not your turn to draw.");
@@ -38,6 +42,7 @@ class Player{
             //checks if they have drawn already
             throw new Error("Drawing is not allowed at this time.");
         }
+        const drawnCard = Deck.draw()
         // Checks if the drawn card is valid
         if (!drawnCard) {
             throw new Error("The deck is empty.");
@@ -56,33 +61,28 @@ class Player{
             throw new Error("Card is not in player's hand.")
         }
         //Checks if it is the player's turn
-        if (this.isTurn) {
-            // Checks if the card is in the player's hand
-            if(!this.hand.includes(card)){
-                throw new Error("Card is not in players hand.")
-            } 
-
-            // Add the card to the temporary hand
-            this.temporaryHand = card;
-
-            // Remove the card from the player's hand
-            this.hand = this.hand.filter(c => c.rank !== card.rank || c.suit !== card.suit);
-
-            // End the player's turn
-            this.isTurn = false;
-
-            this.temporaryHand = null;
-
-            // Allow the player to draw a card
-            this.canDraw = true; // Allow the player to draw a card
-            return card; // reutrn played card
+        if (!this.isTurn) {
+            throw new Error("It is not your turn to play a card.")
         }
-        else{
-            throw new Error("It is not your turn to play a card.");
-        }
+
+        // Add the card to the temporary hand
+        this.temporaryHand = card;
+
+        // Remove the card from the player's hand
+        this.hand = this.hand.filter(c => c !== card);
+
+        // End the player's turn
+        this.isTurn = false;
+
+        // Allow the player to draw a card
+        this.canDraw = true; // Allow the player to draw a card
+        return card; // reutrn played card
     }
     turnToggle(){
-        this.isturn = !this.isturn;
+        this.isTurn = !this.isTurn;
+        if(this.isTurn) {
+            this.canDraw = true;
+        }
     }
 
 }
