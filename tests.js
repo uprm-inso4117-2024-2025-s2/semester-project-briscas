878f2040deaaa27559ab5918590ac2adbbcf9984
MrL9000 <andres.ruiz@upr.edu>
2025-04-09 14:25:04 -0400

    Fully automate the test suite so that one runs a single js file to run the rest of the other js files.

--- tests.js
+++ tests.js
@@ -2,4 +2,13 @@ console.log("Testing project...")
 
 // Insert hooks into existing tests below
 
+const files = require("fs").readdirSync("./tests")
+for(const i in files){
+    var file = files[i];
+    file = file.slice(0,-3)
+    console.log(file);
+    try{require("./tests/"+file);} catch(error){ console.log(error)}
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+}
+
 console.log("Testing completed!")
